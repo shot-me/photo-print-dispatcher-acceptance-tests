87c523bd9e03d3038534c655611a2d5785e6a674
Albert Balinski <albert.balinski@odin360.com>
2019-12-07 10:16:20 +0000

    Add photo model

---
+++ migrations/20191207101004-create-photos.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      originalPhotoUrl: {
+        type: Sequelize.STRING
+      },
+      transformedPhotoUrl: {
+        type: Sequelize.STRING
+      },
+      datePrinted: {
+        type: Sequelize.DATE
+      },
+      dateTransformed: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Photos');
+  }
+};
--- models/photos.js
+++ models/photos.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Photos = sequelize.define('Photos', {
-    originalPhotoUrl: DataTypes.STRING
+    originalPhotoUrl: DataTypes.STRING,
+    transformedPhotoUrl: DataTypes.STRING,
+    datePrinted: DataTypes.DATE,
+    dateTransformed: DataTypes.DATE
   }, {});
   Photos.associate = function(models) {
     // associations can be defined here
